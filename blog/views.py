76344df6d10027565f17696c78a0e9859dd0a97e
RobertJLewis <f3kbwcqc@students.codeinstitute.net>
2025-09-26 13:56:37 +0100

    adding views into project - for authoer and person credentials

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("<h1>Hello, world. You're at the blog index.</h1>")
+posts = [
+    {
+    'author': 'Robert Lewis',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'August 27, 2024'
+    }, 
+    {
+    'author': 'Jane Doe',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'August 28, 2024'
+    }
+]
 
+def home(request):
+    return render(request, 'blog/home.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
