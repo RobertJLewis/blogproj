df18b5514fc1333a115064c61a5c3533664d58ae
RobertJLewis <f3kbwcqc@students.codeinstitute.net>
2025-09-27 20:16:29 +0100

    adding urls into root file

--- blogproj/urls.py
+++ blogproj/urls.py
@@ -26,7 +26,12 @@ urlpatterns = [
     path('', include('blog.urls'))
 
 
-    # user authentications
+     # user authentications
+    path('register/', user_views.register, name="register"),
+    path('profile/', user_views.profile, name="profile"),
+    path('profile/profile_update/', user_views.profile_update, name="profile-update"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout")
 ]
 
 
