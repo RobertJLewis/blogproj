15b7dd32353584470e8654bfc79ab93317099b6f
RobertJLewis <f3kbwcqc@students.codeinstitute.net>
2025-09-27 22:11:04 +0100

    updating views in blog app, and allowing users to delete posts

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    #created_at = models.DateTimeField(default=timezone.now)
-    #updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.titleb
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog-detail', kwargs={'pk': self.pk})
+    
+
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
-
-# Create your views here.
-
+from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return render((request, 'blog/about.html'), {'title': 'About Page'})
-
+    return render(request, 'blog/about.html', {'title': "About Page"})
 
 
 class PostListView(LoginRequiredMixin, ListView):
@@ -22,6 +21,7 @@ class PostListView(LoginRequiredMixin, ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ["-date_posted"]
+
 
 class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
@@ -31,3 +31,38 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # def test_func(self):
+    #     post = self.get_object()
+    #     if self.request.user == post.author:
+    #         return True
+    #     return False
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
