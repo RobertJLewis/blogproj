afda823bd760c1ca379a2295eb1e42601e4da3d3
RobertJLewis <f3kbwcqc@students.codeinstitute.net>
2025-12-18 17:46:47 +0000

    updating aws settings

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -62,12 +62,12 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-north-1')
 AWS_QUERYSTRING_AUTH = False  # public URLs
 
+# 👇 ADD THIS LINE BELOW 👇
+AWS_DEFAULT_ACL = 'public-read'  # <-- This is the fix!
 
 # Tell Django to use S3 for media files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/media/'
-
-
 
 
 
